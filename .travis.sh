78420cb1899965d7d6c3de2d72f7495d8b9edc9c
David Britton <davidpbritton@gmail.com>
2016-11-10 14:22:33 -0700

    Add header for .travis.sh.

--- .travis.sh
+++ .travis.sh
@@ -1,5 +1,19 @@
 #!/bin/sh -e
 
+# Must be run with macaroon to snapcraft store, reference:
+#    https://lists.snapcraft.io/archives/snapcraft/2016-August/000831.html
+#
+# This will be authenticated as you, however will be decrypted
+# only in travis with a secret stored there.
+# 
+#   snapcraft login    # login to Ubuntu SSO
+#   export SNAPCRAFT_SECRET=$(pwgen 20 -1)
+#   export SNAPCRAFT_CONFIG="$(openssl enc -aes-256-cbc -base64 -pass env:SNAPCRAFT_SECRET < ~/.config/snapcraft/snapcraft.cfg)"
+#   # Install travis-ci cli...
+#   travis encrypt SNAPCRAFT_SECRET=$SNAPCRAFT_SECRET -a
+#   travis env set SNAPCRAFT_CONFIG "$SNAPCRAFT_CONFIG"
+
+
 if [ -z "$SNAPCRAFT_SECRET" ]; then
     exit 1
 fi
